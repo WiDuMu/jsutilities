7330efba945d184ee3e660964c8732ddaf45cf76
WiDuMu <97642890+WiDuMu@users.noreply.github.com>
2023-03-29 21:11:23 -0500

    add randomString

---
+++ randomString.js
@@ -0,0 +1,2 @@
+// returns a random string of specified length from the provided character set
+export const randomString = (length, alphabet) => Array.from({length: length}, _ => alphabet[Math.floor(Math.random() * alphabet.length)]).join('')
